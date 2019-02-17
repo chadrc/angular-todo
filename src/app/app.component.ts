20e8fa5287492eb903d816699e70ed3b107f27f0
chadcollins <chadrcollins91@gmail.com>
2019-02-17 16:15:23 -0500

    Moved todo stuff to its component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,77 +9,11 @@ import { TodoService } from "./todo.service";
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.scss"]
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = "Todo App";
   burgerOpen = false;
-  newTodoText = "";
-  selectedListIndex = 0;
-  creatingList = false;
-  listIndexToDelete = -1;
-  editingListIndex = -1;
-  selectedCategoryId = -1;
-  editingCategories = false;
-  currentCategoryPage = 1;
-  categories: Category[] = [];
-  todoLists: TodoList[] = [];
-  fetchCategoriesError: any;
-  fetchTodoListsError: any;
-
-  constructor(private todoService: TodoService) {}
-
-  ngOnInit() {
-    this.todoService.getCategories().subscribe(
-      categories => {
-        this.categories = categories;
-      },
-      error => {
-        this.fetchCategoriesError = error;
-      }
-    );
-
-    this.todoService.getTodoLists().subscribe(
-      todoLists => {
-        this.todoLists = todoLists;
-      },
-      error => {
-        this.fetchTodoListsError = error;
-      }
-    );
-  }
 
   toggleBurger() {
     this.burgerOpen = !this.burgerOpen;
-  }
-
-  get categoriesWithLists() {
-    let categoryIds = {};
-    for (let list of this.todoLists) {
-      categoryIds[list.categoryId] = true;
-    }
-    return Object.keys(categoryIds).map(categoryId =>
-      this.categories.find(category => category.id === parseInt(categoryId))
-    );
-  }
-
-  listsWithCategory(categoryId: number) {
-    return this.todoLists.filter(
-      (list: TodoList) => list.categoryId === categoryId
-    );
-  }
-
-  get selectedListTodos() {
-    if (this.selectedListIndex === -1) {
-      return [];
-    }
-
-    let list = this.todoLists[this.selectedListIndex];
-
-    return list ? list.todos : [];
-  }
-
-  selectList(listId: number) {
-    this.selectedListIndex = this.todoLists.findIndex(
-      (list: TodoList) => list.id === listId
-    );
   }
 }
--- src/app/todo/todo-page.component.ts
+++ src/app/todo/todo-page.component.ts
@@ -1,15 +1,78 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import Category from "src/classes/Category";
+import TodoList from "src/classes/TodoList";
+import { TodoService } from "../todo.service";
 
 @Component({
-  selector: 'app-todo-page',
-  templateUrl: './todo-page.component.html',
-  styleUrls: ['./todo-page.component.scss']
+  selector: "app-todo-page",
+  templateUrl: "./todo-page.component.html",
+  styleUrls: ["./todo-page.component.scss"]
 })
 export class TodoPageComponent implements OnInit {
+  newTodoText = "";
+  selectedListIndex = 0;
+  creatingList = false;
+  listIndexToDelete = -1;
+  editingListIndex = -1;
+  selectedCategoryId = -1;
+  editingCategories = false;
+  currentCategoryPage = 1;
+  categories: Category[] = [];
+  todoLists: TodoList[] = [];
+  fetchCategoriesError: any;
+  fetchTodoListsError: any;
 
-  constructor() { }
+  constructor(private todoService: TodoService) {}
 
   ngOnInit() {
+    this.todoService.getCategories().subscribe(
+      categories => {
+        this.categories = categories;
+      },
+      error => {
+        this.fetchCategoriesError = error;
+      }
+    );
+
+    this.todoService.getTodoLists().subscribe(
+      todoLists => {
+        this.todoLists = todoLists;
+      },
+      error => {
+        this.fetchTodoListsError = error;
+      }
+    );
   }
 
+  get categoriesWithLists() {
+    let categoryIds = {};
+    for (let list of this.todoLists) {
+      categoryIds[list.categoryId] = true;
+    }
+    return Object.keys(categoryIds).map(categoryId =>
+      this.categories.find(category => category.id === parseInt(categoryId))
+    );
+  }
+
+  listsWithCategory(categoryId: number) {
+    return this.todoLists.filter(
+      (list: TodoList) => list.categoryId === categoryId
+    );
+  }
+
+  get selectedListTodos() {
+    if (this.selectedListIndex === -1) {
+      return [];
+    }
+
+    let list = this.todoLists[this.selectedListIndex];
+
+    return list ? list.todos : [];
+  }
+
+  selectList(listId: number) {
+    this.selectedListIndex = this.todoLists.findIndex(
+      (list: TodoList) => list.id === listId
+    );
+  }
 }
